f9ecc403f985ab243e4e2fd5de330a8ff270be92
Ryan Lovelett <ryan@lovelett.me>
2013-07-22 12:12:08 -0500

    Fix startup lag; remove dead code

--- SampleTimer/main.m
+++ SampleTimer/main.m
@@ -13,18 +13,6 @@
 
 int main(int argc, char *argv[])
 {
-    TimeSheet* instanceOfTimeSheet = [[TimeSheet alloc] init];
-    
-    [instanceOfTimeSheet start];
-    
-    sleep(2); // sleep for 2s
-    
-    NSLog(@"[Running] %@", [instanceOfTimeSheet getElapsedTime:@"ss.SSSS"]);
-    
-    [instanceOfTimeSheet stop];
-    
-    NSLog(@"[Total Elapsed] %@", [instanceOfTimeSheet getElapsedTime:@"ss.SSSS"]);
-    
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([SplitAppDelegate class]));
     }
